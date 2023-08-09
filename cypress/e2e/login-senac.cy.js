ca72befd760520ad2baa1eb0ebe9a8cda524e49b
luiszinhob <luisfelipeoliveiralima615@gmail.com>
2023-08-09 20:43:53 -0300

    cypress

---
+++ cypress/e2e/login-senac.cy.js
@@ -0,0 +1,13 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.sp.senac.br/')
+
+    cy.get('.mburger').click()
+
+    cy.get('#txtLoginNaoLogadoMob').click()
+
+    cy.get('#login-email').type('sadye1823@uorak.com')
+    cy.get('#login-password').type("sajdsajdsioahosaho")
+    cy.get('#btnLoginHeader').click()
+  })
+})
